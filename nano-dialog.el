610eb6af89904a7725053d50478590caed20606e
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2023-04-16 20:33:24 +0200

    Added license + package information

--- nano-dialog.el
+++ nano-dialog.el
@@ -1,3 +1,45 @@
+;;; mini-dialog.el --- Native diaglob popups -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023  Nicolas P. Rougier
+
+;; Maintainer: Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
+;; URL: https://github.com/rougier/nano-dialog
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This package provide native dialog boxes.
+;;
+;;; Usage:
+;;
+;; (nano-dialog "*nano-dialog*"
+;;              :title "􁌴 NANO Dialog"
+;;              :buttons '("OK" "CANCEL"))
+;;
+;;
+;; NEWS:
+;;
+;; Version 0.1
+;; - First version
+
+;;; Code
 (require 'svg-lib)
 
 ;; See https://material.io/design/color/the-color-system.html
@@ -447,11 +489,8 @@
   (apply #'nano-dialog buffer
          :face 'nano-dialog-failure-face args))
 
-(nano-dialog "*nano-dialog*"
-              :transient nil
-              :margin '(2 . 2)
-              :title "􁌴 NANO Dialog"
-              :buttons '("OK" "CANCEL"))
+(provide 'nano-dialog)
+;;; nano-dialog.el ends here
 
 ;; (nano-dialog-info     "*nano-dialog*" :title "􁌴 Info")
 ;; (nano-dialog-question "*nano-dialog*" :title "􁌶 Question")
